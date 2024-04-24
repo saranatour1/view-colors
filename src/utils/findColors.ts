8fa0024e6620e2d10f6c4407bef318a33e562146
saranatour1 <saranatourcontact@gmail.com>
2024-04-24 12:15:39 +0300

    used `CSS.supports` instead of regex

--- src/utils/findColors.ts
+++ src/utils/findColors.ts
@@ -1,32 +1,5 @@
 export const findColors = (value:string)=>{
-
-  const hexRegex = /#([a-f0-9]{3}|[a-f0-9]{4}(?:[a-f0-9]{2}){0,2})\b/gi;
-
-  const hexaRegex = /#([a-f0-9]{8}|[a-f0-9]{4})\b/gi;
-  
-  const hslRegex = /hsl\(\s*(\d+)\s*,\s*(\d*(?:\.\d+)?%)\s*,\s*(\d*(?:\.\d+)?%)\)/gi;
-  
-  const hslaRegex = /hsla\((\d+),\s*([\d.]+)%,\s*([\d.]+)%,\s*(\d*(?:\.\d+)?)\)/gi;
-  
-  const rgbaRegex = /rgba\((\d{1,3}%?),\s*(\d{1,3}%?),\s*(\d{1,3}%?),\s*(\d*(?:\.\d+)?)\)/gi;
-  
-  const rgbRegex = /rgb\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3})\)/gi;
-
-
-  const colorsObj: string[] | any = [];
-  const variableNames = [
-    hexRegex,
-    hexaRegex,
-    hslRegex,
-    hslaRegex,
-    rgbaRegex,
-    rgbRegex
-  ];
-  variableNames.forEach((variable) =>
-    colorsObj.push(value.match(variable) || [])
-  );
-  
-  return colorsObj.flat().filter(Boolean);
+  return value.split(/\s+/).filter(word => CSS.supports('color', word))
 }
 
 
